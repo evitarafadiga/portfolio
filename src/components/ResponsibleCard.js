d20edae349efd395f8f8faa3cf4f3c6ff0564d2f
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2023-09-25 19:46:41 -0300

    fixed design
    icons added, links added, fixed some design

--- src/components/ResponsibleCard.js
+++ src/components/ResponsibleCard.js
@@ -6,9 +6,17 @@ export const ResponsibleCard = ({ img, title, subtitle, desc, alt, href }) => {
                     <img className="h-48 w-full object-cover md:h-full md:w-48" src={img} alt={alt} />
                 </div>
                 <div className="p-8">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title}</div>
-                    <a href={href} className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{subtitle}</a>
-                    <p className="mt-2 text-slate-500">{desc}</p>
+                    <div className=" text-justify">
+                        <a href={href} className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{subtitle}</a>
+                        <p className="mt-2 text-slate-500">{desc}</p>
+                    </div>
+                    <div className="flex flex-row pt-10 uppercase tracking-wide text-justify align-center place-items-center">
+                        <div>
+                            <p className="text-xs text-slate-400 pr-4">stacks</p>
+                        </div>
+                        <div className="text-sm text-indigo-500 font-semibold">{title}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
